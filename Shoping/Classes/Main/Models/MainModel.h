2a311e1772b1f908acca7c1d9afca5b1dce5c606
韩苇棋 <hanweiqi@mail.com>
2016-03-21 08:38:45 +0800

    zhuyemian

---
+++ Shoping/Classes/Main/Models/MainModel.h
@@ -0,0 +1,13 @@
+//
+//  MainModel.h
+//  Shoping
+//
+//  Created by scjy on 16/3/21.
+//  Copyright © 2016年 韩苇棋. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MainModel : NSObject
+
+@end
---
+++ Shoping/Classes/Main/Models/MainModel.m
@@ -0,0 +1,13 @@
+//
+//  MainModel.m
+//  Shoping
+//
+//  Created by scjy on 16/3/21.
+//  Copyright © 2016年 韩苇棋. All rights reserved.
+//
+
+#import "MainModel.h"
+
+@implementation MainModel
+
+@end
